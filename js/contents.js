44a65a47ce5597abb37590e46b1ac6ffd7d7aa61
Dyvak Yurii <dyvakyurii@gmail.com>
2019-10-14 01:43:55 +0300

    Changed design, added photo

--- js/contents.js
+++ js/contents.js
@@ -1,13 +1,27 @@
-function expandGroups(event) {
-    var els = document.getElementsByClassName('title');
-    if (event === 'touchend') {
-        event.preventDefault();
-    }
-    Array.prototype.forEach.call(els, function (el) {
-        el.addEventListener(event, function () {
-            el.parentElement.classList.toggle('expand');
-            if (el.classList.contains('flash')) {
-                el.classList.remove('flash');
+function navigation(event) {
+    let buttons = document.getElementsByTagName('button');
+    setButtonEventListeners(event, buttons);
+}
+
+function setButtonEventListeners(event, buttons) {
+    Array.prototype.forEach.call(buttons, function (button) {
+        button.addEventListener(event, function () {
+            let activeBlock = button.id + '-frame';
+            let block = document.getElementById(activeBlock);
+            let content = document.getElementById('content');
+            content.innerHTML = block.innerHTML;
+            let titles = document.getElementsByClassName('title');
+            setTitleEventListeners(event, titles);
+        }, false);
+    });
+}
+
+function setTitleEventListeners(event, titles) {
+    Array.prototype.forEach.call(titles, function (title) {
+        title.addEventListener(event, function () {
+            title.parentElement.classList.toggle('expand');
+            if (title.classList.contains('flash')) {
+                title.classList.remove('flash');
             }
         }, false);
     });
@@ -15,97 +29,141 @@ function expandGroups(event) {
 
 // build headers for requests
 function buildHeader() {
-    var head = new Headers();
+    let head = new Headers();
     head.append('pragma', 'no-cache');
     head.append('cache-control', 'no-cache');
 
-    var init = {
+    let init = {
         method: 'GET',
         headers: head
     };
     return init;
 }
 
-// pull highlights content
-function pullEducation() {
-    return fetch('contents/education.json', buildHeader()).then(function (response) {
-        return response.json();
-    });
-}
-
-// pull highlights content
-function pullWork() {
-    return fetch('contents/work.json', buildHeader()).then(function (response) {
-        return response.json();
-    });
-}
-
-// pull press links
-function pullPress() {
-    return fetch('contents/projects.json', buildHeader()).then(function (response) {
-        return response.json();
-    });
+function pull(path) {
+    return fetch(path, buildHeader())
+        .then(function (response) {
+            return response.json();
+        });
 }
 
 function education() {
-    var h = document.getElementById('education');
-    pullEducation().then(function (r) {
-        document.getElementsByClassName('loading')[0].classList.add('hide');
-        r.data.forEach(function (x) {
-            h.innerHTML += '<li class="' + x.type +
-                '"><div class="left">' + x.date +
-                '</div><div class="desc"><div>' + x.description.what + ' ' +
-                '<em>' + x.description.emphasis + '</em></div><div class="info">' +
-                x.description.info + '</div></div></li>';
+    let h = document.getElementById('education-block');
+    pull('contents/education.json')
+        .then(function (response) {
+            document.getElementsByClassName('loading')[0].classList.add('hide');
+            console.log(JSON.stringify(response.data))
+            response.data.forEach(function (x) {
+                h.innerHTML += '<li class="' + x.type +
+                    '"><div class="left">' + x.date +
+                    '</div><div class="desc"><div>' + x.description.what + ' ' +
+                    '<em>' + x.description.emphasis + '</em></div><div class="info">' +
+                    x.description.info + '</div></div></li>';
+            });
         });
-    });
 }
 
 function work() {
-    var h = document.getElementById('work');
-    pullWork().then(function (r) {
-        document.getElementsByClassName('loading')[1].classList.add('hide');
-        r.data.forEach(function (x) {
-            h.innerHTML += '<li class="' + x.type +
-                '"><div class="left">' + x.date +
-                '</div><div class="desc"><div>' + x.description.what + ' ' +
-                '<em>' + x.description.emphasis + '</em></div><div class="info">' +
-                x.description.info + '</div></div></li>';
+    let h = document.getElementById('work-block');
+    pull('contents/work.json')
+        .then(function (response) {
+            document.getElementsByClassName('loading')[1].classList.add('hide');
+            response.data.forEach(function (x) {
+                h.innerHTML += '<li class="' + x.type +
+                    '"><div class="left">' + x.date +
+                    '</div><div class="desc"><div>' + x.description.what + ' ' +
+                    '<em>' + x.description.emphasis + '</em></div><div class="info">' +
+                    x.description.info + '</div></div></li>';
+            });
         });
+}
+
+function internship() {
+    let h = document.getElementById('internship-block');
+    pull('contents/internship.json')
+        .then(function (response) {
+            document.getElementsByClassName('loading')[2].classList.add('hide');
+            response.data.forEach(function (x) {
+                h.innerHTML += '<li class="' + x.type +
+                    '"><div class="left">' + x.date +
+                    '</div><div class="desc"><div>' + x.description.what + ' ' +
+                    '<em>' + x.description.emphasis + '</em></div><div class="info">' +
+                    x.description.info + '</div></div></li>';
+            });
+        });
+}
+
+function courses() {
+    let h = document.getElementById('courses-block');
+    pull('contents/courses.json')
+        .then(function (response) {
+            document.getElementsByClassName('loading')[3].classList.add('hide');
+            response.data.forEach(function (x) {
+                h.innerHTML += '<li class="' + x.type +
+                    '"><div class="left">' + x.date +
+                    '</div><div class="desc"><div>' + x.description.what + ' ' +
+                    '<em>' + x.description.emphasis + '</em></div><div class="info">' +
+                    x.description.info + '</div></div></li>';
+            });
+        });
+}
+
+function hobbies() {
+    let h = document.getElementById('hobbies-block');
+    pull('contents/hobbies.json')
+        .then(function (response) {
+            document.getElementsByClassName('loading')[4].classList.add('hide');
+            response.data.forEach(function (x) {
+                h.innerHTML += '<li class="' + x.type +
+                    '"><div class="desc"><div>' + x.description.what + ' ' +
+                    '<em>' + x.description.emphasis + '</em></div><div class="info">' +
+                    x.description.info + '</div></div></li>';
+            });
+        });
+}
+
+// generate projects section
+function projects(articles) {
+    let p = document.getElementById('projects-block');
+    let load = document.getElementsByClassName('loading')
+    console.log(JSON.stringify(load))
+    document.getElementsByClassName('loading')[5].classList.add('hide');
+    articles.forEach(function (item) {
+        p.innerHTML += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
     });
 }
 
 
-// first 5 press articles
+
+
+// first 5 projects articles
 function pressVars() {
-    var recent = [];
+    let recent = [];
 
-    pullPress().then(function (r) {
-        for (var i = 0; i < 20; i++) {
-            recent[i] = r.data[i];
-        }
-    })
+    pull('contents/projects.json')
+        .then(function (response) {
+            for (let i = 0; i < 20; i++) {
+                if (response.data[i]) {
+                    recent[i] = response.data[i];
+                }
+            }
+        })
         .then(function () {
-            press(recent);
+            projects(recent);
         });
 }
 
-// generate press section
-function press(articles) {
-    var p = document.getElementById('projects');
-    document.getElementsByClassName('loading')[2].classList.add('hide');
-    articles.forEach(function (x) {
-        p.innerHTML += '<li><a href="' + x.url + '">' + x.title + '</a></li>';
-    });
-}
-
-// get all the contents jazz
+// get all the contents
 (function () {
     window.addEventListener('load', function () {
+
         education();
-        work()
+        work();
+        internship();
+        courses();
+
         pressVars();
-        expandGroups('click');
-        expandGroups('touchend');
+        navigation('click');
+
     }, false);
 })();
