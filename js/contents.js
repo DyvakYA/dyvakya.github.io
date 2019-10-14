59eb766b9b2b6222bace2507bdcf57f5d9511c89
Dyvak Yurii <dyvakyurii@gmail.com>
2019-10-14 16:50:53 +0300

    Rewrote some styles, added content, couple photos

--- js/contents.js
+++ js/contents.js
@@ -27,6 +27,40 @@ function setTitleEventListeners(event, titles) {
     });
 }
 
+/* Dark Mode */
+// function setModeEventListener() {
+//     let list = document.body.classList;
+//     document.getElementById('toggler').addEventListener('change', event => {
+//         event.target.checked ? list.add('dark-mode') : list.remove('dark-mode');
+//     });
+// }
+
+function setBioEventListener() {
+    Array.from(document.getElementsByTagName('button')).forEach(e => {
+        e.addEventListener('click', bioToggle);
+    });
+}
+
+function bioToggle(e) {
+    let bioType = e.target;
+    let color = 'rgba(150, 151, 156, 0.1)';
+    off(bioType);
+    bioType.style.cssText = `background: ${color}; font-weight: bold;`;
+    //document.getElementsByClassName(bioType.id)[0].classList.add('show');
+}
+
+function off(bioType) {
+    Array.from(document.getElementsByTagName('button')).forEach(butt => {
+        butt.style.borderColor = '#96979c';
+        butt.style.color = '#96979c';
+        butt.style.background = '';
+    });
+    Array.from(document.getElementsByClassName('bio')).forEach(e => {
+        e.classList.remove('show');
+    });
+}
+
+
 // build headers for requests
 function buildHeader() {
     let head = new Headers();
@@ -112,11 +146,17 @@ function hobbies() {
     pull('contents/hobbies.json')
         .then(function (response) {
             document.getElementsByClassName('loading')[4].classList.add('hide');
+            console.log(JSON.stringify(response.data));
             response.data.forEach(function (x) {
+                // h.innerHTML += '<li class="' + x.type +
+                //     '"><div class="desc"><div>' + x.description.what + ' ' +
+                //     '<em>' + x.description.emphasis + '</em></div><div class="info">' +
+                //     x.description.info + '</div></div></li>';
+
                 h.innerHTML += '<li class="' + x.type +
-                    '"><div class="desc"><div>' + x.description.what + ' ' +
-                    '<em>' + x.description.emphasis + '</em></div><div class="info">' +
-                    x.description.info + '</div></div></li>';
+                    '"><div class="desc"><div>' + x.description.title + ' ' +
+                    '<img class="photo" src="' + x.description.photo + '"></div></li>'
+                console.log(h.innerHTML);
             });
         });
 }
@@ -124,15 +164,16 @@ function hobbies() {
 // generate projects section
 function projects(articles) {
     let p = document.getElementById('projects-block');
-    let load = document.getElementsByClassName('loading')
     document.getElementsByClassName('loading')[5].classList.add('hide');
     articles.forEach(function (item) {
         p.innerHTML += '<li><a href="' + item.url + '">' + item.title + '</a></li>';
     });
 }
 
-
-
+function setRandomPhoto() {
+    let num = Math.floor(Math.random() * 3) + 1;
+    document.getElementById('picture').src = `img/photos/photo_${num}.jpg`;
+}
 
 // first 5 projects articles
 function pressVars() {
@@ -159,9 +200,19 @@ function pressVars() {
         work();
         internship();
         courses();
+        hobbies();
 
         pressVars();
         navigation('click');
+
+        //setModeEventListener();
+        setBioEventListener();
+
+        setRandomPhoto();
+
+        setInterval(() => {
+            setRandomPhoto();
+        }, 2500);
 
     }, false);
 })();
